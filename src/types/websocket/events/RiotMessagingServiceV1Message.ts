c1d8bd20adcf00bcfdd44b7f87574bca411b5095
techchrism <chris@techchrism.me>
2022-10-16 23:08:01 -0700

    add basic interface for RiotMessagingServiceV1MessageData

--- src/types/websocket/events/RiotMessagingServiceV1Message.ts
+++ src/types/websocket/events/RiotMessagingServiceV1Message.ts
@@ -1,7 +1,49 @@
 import {WebSocketEvent} from '../WebSocketEvent'
 
+//TODO refactor with service-specific payload data
 interface RiotMessagingServiceV1MessageData {
-    stuff: string
+    ackRequired: boolean
+    id: ''
+    payload: '' | {
+        subject: string
+        cxnState: 'CONNECTED'
+        clientID: string
+        clientVersion: string
+        loopState: string
+        loopStateMetadata: string
+        version: number
+        lastHeartbeatTime: string
+        expiredTime: string
+        heartbeatIntervalMillis: number
+        playtimeNotification: ""
+        playtimeMinutes: number
+        isRestricted: boolean
+        userinfoValidTime: string
+        restrictionType: string
+        clientPlatformInfo: {
+            platformType: 'PC'
+            platformOS: 'Windows'
+            platformOSVersion: string
+            platformChipset: string
+        }
+    } | {
+        balances: {
+            [key: string]: {
+                amount: number
+                version: number
+                active: boolean
+            }
+        }
+        deltas: {
+            [key: string]: {
+                amount: number
+            }
+        }
+    }
+    resource: string
+    service: 'pregame' | 'core-game' | 'parties' | 'match-details' | 'mmr' | 'account-xp' | 'contracts' | 'cap.counters' | 'session'
+    timestamp: number
+    version: string
 }
 
 export type RiotMessagingServiceV1Message = WebSocketEvent<RiotMessagingServiceV1MessageData> & {
