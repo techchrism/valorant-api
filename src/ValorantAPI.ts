142f0a3225c2f04257992c1327b50b1274b09e81
techchrism <chris@techchrism.me>
2022-10-16 21:17:31 -0700

    emit translated and processed websocket events

--- src/ValorantAPI.ts
+++ src/ValorantAPI.ts
@@ -1,13 +1,16 @@
 import RequestMaker from './RequestMaker'
 import {EventEmitter, EventKey} from 'ee-ts'
 import {WebSocketEventEmitters} from './types/websocket/WebSocketEventTypes'
-import {WebSocket} from 'isomorphic-ws'
+import {MessageEvent, WebSocket} from 'isomorphic-ws'
 
 type ValorantAPIEvents = {
     websocketOpen: (websocket: WebSocket) => void
     websocketClose: () => void
 }
 
+/**
+ * Translates the event name from ValorantAPI (this project) to the internal event name used by Valorant
+ */
 function websocketEventToRiotName(eventName: EventKey<WebSocketEventEmitters>): string {
     switch(eventName) {
         case 'websocketRiotMessagingServiceV1Message': return 'OnJsonApiEvent_riot-messaging-service_v1_message'
@@ -29,7 +32,7 @@ export class ValorantAPI extends EventEmitter<CombinedEventType> {
 
     private _websocketEventCount = 0
     private _ws?: WebSocket
-    private _subscribedEvents = new Set<string>()
+    private _subscribedEvents = new Map<string, EventKey<WebSocketEventEmitters>>()
 
     constructor(requestMaker: RequestMaker) {
         super()
@@ -47,13 +50,30 @@ export class ValorantAPI extends EventEmitter<CombinedEventType> {
         this._ws.on('close', () => {
             this.emit('websocketClose')
         })
+        this._ws.onmessage = (event: MessageEvent) => {
+            if(typeof event.data === 'string') {
+                const data = JSON.parse(event.data)
+                if(Array.isArray(data) && data.length === 3) {
+                    const [id, eventName, payload] = data
+                    if(this._subscribedEvents.has(eventName)) {
+                        let processedPayload = payload
+                        if(typeof processedPayload === 'string') {
+                            try {
+                                processedPayload = JSON.parse(processedPayload)
+                            } catch(ignored) {}
+                        }
+                        this.emit(this._subscribedEvents.get(eventName)!, processedPayload)
+                    }
+                }
+            }
+        }
     }
 
     override _onEventHandled(key: EventKey<CombinedEventType>) {
         if(isWebsocketEvent(key)) {
             this._websocketEventCount++
             const riotName = websocketEventToRiotName(key)
-            this._subscribedEvents.add(riotName)
+            this._subscribedEvents.set(riotName, key)
 
             if(this._ws === undefined || this._ws.readyState === WebSocket.CLOSED) {
                 this.connect()
